ae741176d4a3562bcba7c8ca1464c856ee865690
Chris Malton <chris@swlines.co.uk>
2016-08-19 16:34:07 +0100

    Add support for creating a service for additional instances.

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -131,4 +131,23 @@ define powerdns::instance(
       value => $setgid,
     }
   }
+
+  if($instance_name != "default"){
+    $service_name   = pick($::powerdns::service_name,   'pdns')
+    $_service_name  = "${service_name}@${instance_name" 
+    $service_ensure = pick($::powerdns::service_ensure, 'running')
+    $service_enable = pick($::powerdns::service_enable, true)
+
+    validate_string($_service_name)
+
+    validate_string($service_ensure)
+    validate_re($service_ensure, '^(running|stopped|[._0-9a-zA-Z:-]+)$')
+
+    validate_bool($service_enable)
+
+    service { $_service_name:
+        ensure => $service_ensure,
+        enable => $service_enable,
+    }
+  }
 }
